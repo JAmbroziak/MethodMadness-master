1ee159faa477b86d7213f338b82af383c794d6fd
BT_1N3_03 <jambroziak9340@bths.edu>
2018-10-03 16:48:55 -0400

    isPalindrome, dateStr, cutOut

--- src/JAmbroziakLib.java
+++ src/JAmbroziakLib.java
@@ -1,14 +1,31 @@
 public class JAmbroziakLib {
 
-    public static boolean isPrime(int n)
+    public static boolean isPalindrome(String str)
     {
-        for (int i = 2; i<n; i++)
+        String pDrome = "";
+        for(int i = str.length(); i > 0; i--)
         {
-            if (n%1 == 0)
-            {
-                return false;
-            }
+            pDrome += str.substring(i-1, i);
         }
-        return true;
+        if (str.equals(pDrome))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static String dateStr(String date)
+    {
+        String mm = date.substring(0,2);
+        String dd = date.substring(3,5);
+        String yyyy = date.substring(6);
+        return (dd + " - " + mm + " - " + yyyy);
+    }
+
+    public static String cutOut(String main, String sub)
+    {
+        int subStart = main.indexOf(sub);
+        int subEnd = subStart + sub.length();
+        return (main.substring(0, subStart) + main.substring(subEnd));
     }
 }
--- src/TestFile.java
+++ src/TestFile.java
@@ -4,7 +4,8 @@ public class TestFile {
     {
 
         System.out.println("Peter Piper picked a peck of pickled peppers");
-        System.out.println(JAmbroziakLib.isPrime(13));
-        System.out.println(JAmbroziakLib.isPrime(23));
+        System.out.println(JAmbroziakLib.isPalindrome("racecar"));
+        System.out.println(JAmbroziakLib.dateStr("03/13/2001"));
+        System.out.println(JAmbroziakLib.cutOut("walkie-talkie", "alk"));
     }
 }
