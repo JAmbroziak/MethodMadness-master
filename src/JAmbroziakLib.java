1b066190b3ced372e8c0c77212b4d320050fc9b9
fpsft <jambroziak9340@bths.edu>
2018-10-06 17:51:52 -0400

    isFibonacci

--- src/JAmbroziakLib.java
+++ src/JAmbroziakLib.java
@@ -1,5 +1,9 @@
 public class JAmbroziakLib {
-
+    /**
+     * Checks a word to see if it is a palindrome or not
+     * @param str the word to check
+     * @return a boolean value: true if palindrome, false if not
+     */
     public static boolean isPalindrome(String str)
     {
         String pDrome = "";
@@ -14,6 +18,11 @@ public class JAmbroziakLib {
         return false;
     }
 
+    /**
+     * reformats a (mm/dd/yyyy) date into (dd - mm - yyyy)
+     * @param date a date in the (mm/dd/yyyy) format
+     * @return a string of the date in (dd - mm - yyyy) format
+     */
     public static String dateStr(String date)
     {
         String mm = date.substring(0,2);
@@ -22,17 +31,28 @@ public class JAmbroziakLib {
         return (dd + " - " + mm + " - " + yyyy);
     }
 
+    /**
+     * removes the first occurence of String sub from String main
+     * @param main the main string
+     * @param sub the string to be removed from String main
+     * @return String main with the first occurence of String sub removed from it
+     */
     public static String cutOut(String main, String sub)
     {
         int subStart = main.indexOf(sub);
         int subEnd = subStart + sub.length();
-        if(main.indexOf(sub) == -1)
+        if(!main.contains(sub))
         {
             return main;
         }
         return (main.substring(0, subStart) + main.substring(subEnd));
     }
 
+    /**
+     * Adds positive integers together from 0 to n
+     * @param n the number of sequential integers
+     * @return int value of the sum
+     */
     public static int sumUpTo(int n)
     {
         int sum = 0;
@@ -43,17 +63,26 @@ public class JAmbroziakLib {
         return sum;
     }
 
-    public static int isFibonacci(int n)
+    /**
+     * checks if int n is a number in the Fibonacci sequence
+     * @param n the number to be checked
+     * @return a boolean value: true if n is a Fibonacci number, false if not
+     */
+    public static boolean isFibonacci(int n)
     {
-        int a = 0;
-        int b = 1;
+        int a = 1;
+        int b = 0;
         int fibo = 0;
-        for(int i = 0; i<n-1; i++)
+        while(fibo < n)
         {
-            fibo = (a+b);
-            a = b;
-            b = fibo;
+            fibo = a+b;
+            b = a;
+            a = fibo;
+            if(fibo == n)
+            {
+                return true;
+            }
         }
-        return fibo;
+        return false;
     }
 }
--- src/TestFile.java
+++ src/TestFile.java
@@ -4,10 +4,10 @@ public class TestFile {
     {
 
         System.out.println("Peter Piper picked a peck of pickled peppers");
-        System.out.println(JAmbroziakLib.isPalindrome("racecar"));
+        System.out.println(JAmbroziakLib.isPalindrome("tictac"));
         System.out.println(JAmbroziakLib.dateStr("03/13/2001"));
         System.out.println(JAmbroziakLib.cutOut("walkie-talkie", "walk"));
         System.out.println(JAmbroziakLib.sumUpTo(5));
-        System.out.println(JAmbroziakLib.isFibonacci(6));
+        System.out.println(JAmbroziakLib.isFibonacci(144));
     }
 }
