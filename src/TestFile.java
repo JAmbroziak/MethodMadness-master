d4961cb1d3e2ce5a80ba1a79351c5b3ed8f83989
fpsft <jambroziak9340@bths.edu>
2018-10-07 20:31:47 -0400

    isFibonacci

--- src/TestFile.java
+++ src/TestFile.java
@@ -4,10 +4,10 @@ public class TestFile {
     {
 
         System.out.println("Peter Piper picked a peck of pickled peppers");
-        System.out.println(JAmbroziakLib.isPalindrome("tictac"));
+        System.out.println(JAmbroziakLib.isPalindrome("TicTac"));
         System.out.println(JAmbroziakLib.dateStr("03/13/2001"));
         System.out.println(JAmbroziakLib.cutOut("walkie-talkie", "walk"));
-        System.out.println(JAmbroziakLib.sumUpTo(5));
-        System.out.println(JAmbroziakLib.isFibonacci(144));
+        System.out.println(JAmbroziakLib.sumUpTo(7));
+        System.out.println(JAmbroziakLib.isFibonacci(21));
     }
 }
